35c46c74b4cfb6738f3fa6110cd08b12d3d8c3f2
havidad <havidad@ubuntuandthangs>
2015-11-06 18:02:28 -0500

    Changed Background Color on Selection Screen

--- core/src/com/hypoxiagames/marioclone/screens/SaveSelectionScreen.java
+++ core/src/com/hypoxiagames/marioclone/screens/SaveSelectionScreen.java
@@ -15,7 +15,7 @@ public class SaveSelectionScreen implements com.badlogic.gdx.Screen {
 	}
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 		
 	}
